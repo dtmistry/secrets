f4772274f4cca914ef37cfca0a9ac18635a10191
Deep Mistry <dmistry@cars.com>
2017-08-13 22:57:09 -0500

    docs

--- secrets.go
+++ secrets.go
@@ -18,10 +18,12 @@ const (
 var ErrMissingLocation = errors.New("location to read secrets from is required")
 var ErrMissingSecretName = errors.New("secretName is required")
 
+// API to read Docker secrets
 type Secrets struct {
 	Location string
 }
 
+// readLines returns the lines from a file
 func (s *Secrets) readLines(fileName string) ([]string, error) {
 	var lines []string
 	f, err := os.Open(filepath.Join(s.Location, fileName))
@@ -39,6 +41,7 @@ func (s *Secrets) readLines(fileName string) ([]string, error) {
 	return lines, nil
 }
 
+// readFile returns the contents of a file as string
 func (s *Secrets) readFile(fileName string) (string, error) {
 	data, err := ioutil.ReadFile(filepath.Join(s.Location, fileName))
 	if err != nil {
@@ -51,6 +54,7 @@ func (s *Secrets) readFile(fileName string) (string, error) {
 	return string(data), nil
 }
 
+// ReadAsMap returns a Docker secret as a Map. The secret to be read must be created with a properties file format
 func (s *Secrets) ReadAsMap(secretName string) (map[string]string, error) {
 	secret := make(map[string]string)
 	if len(secretName) == 0 {
@@ -71,6 +75,7 @@ func (s *Secrets) ReadAsMap(secretName string) (map[string]string, error) {
 	return secret, nil
 }
 
+// Read returns a Docker secret string
 func (s *Secrets) Read(secretName string) (string, error) {
 	var secret string
 	if len(secretName) == 0 {
@@ -79,6 +84,7 @@ func (s *Secrets) Read(secretName string) (string, error) {
 	return s.readFile(secretName)
 }
 
+// Creates a new Secrets struct with the a custom Docker secrets location
 func NewSecrets(location string) (*Secrets, error) {
 	if len(location) == 0 {
 		return nil, ErrMissingLocation
@@ -88,6 +94,7 @@ func NewSecrets(location string) (*Secrets, error) {
 	}, nil
 }
 
+// Creates a new Secrets struct with the default Docker secrets location of /run/secrets/
 func NewDefaultSecrets() *Secrets {
 	return &Secrets{
 		Location: DEFAULT_SECRETS_LOCATION,
